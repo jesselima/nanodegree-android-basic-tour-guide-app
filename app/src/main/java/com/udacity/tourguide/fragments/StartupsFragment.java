7adcd4fc6a88f6390ed09203492c22cd7673a318
jesselima <jesselima.tech@gmail.com>
2018-05-19 15:38:39 -0300

    Strings of startups data moved to string.xml

--- app/src/main/java/com/udacity/tourguide/fragments/StartupsFragment.java
+++ app/src/main/java/com/udacity/tourguide/fragments/StartupsFragment.java
@@ -36,93 +36,94 @@ public class StartupsFragment extends Fragment {
 
         final ArrayList<Startup> startups = new ArrayList<Startup>();
         startups.add(new Startup(
-                "Google",
-                "Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software and hardware",
+                getString(R.string.name_google),
+                getString(R.string.description_google),
                 R.drawable.logo_google,
                 37.4220041,
                 -122.0862462,
                 1998,
-                "Larry Page, Sergey Brin",
-                "www.google.com",
-                "Internet, Software, Computer Hardware",
-                "1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA"));
+                getString(R.string.founders_google),
+                getString(R.string.site_google),
+                getString(R.string.industry_google),
+                getString(R.string.address_google)));
         startups.add(new Startup(
-                "Udacity",
-                "Udacity is an e-learning platform that offers credential programs in artificial intelligence, machine learning, and robotics.",
+                getString(R.string.name_udacity),
+                getString(R.string.description_udacity),
                 R.drawable.logo_udacity,
                 37.3999172,
                 -122.1105517,
                 2011,
-                "David Stavens, Mike Sokolsky, Sebastian Thrun",
-                "www.udacity.com",
-                "E-Learning, EdTech, Education, Internet",
-                "2465 Latham St, Mountain View, CA 94040, USA"));
+                getString(R.string.founders_udacity),
+                getString(R.string.site_udacity),
+                getString(R.string.industry_udacity),
+                getString(R.string.address_udacity)));
 
         startups.add(new Startup(
-                "Linkedin",
-                "LinkedIn is a business and employment-oriented service that operates via websites and mobile apps. It is mainly used for professional networking, including employers posting jobs and job seekers posting their CVs.",
+                getString(R.string.name_linkedin),
+                getString(R.string.description_linkedin),
                 R.drawable.logo_linkedin,
                 37.3926882,
                 -122.0442225,
                 2002,
-                "Reid Hoffman",
-                "www.linkedin.com",
-                "Internet",
-                "1000 W Maude Ave, Sunnyvale, CA 94085, USA"));
+                getString(R.string.founders_linkedin),
+                getString(R.string.site_linkedin),
+                getString(R.string.industry_linkedin),
+                getString(R.string.address_linkedin)));
         startups.add(new Startup(
-                "Elastic",
-                "Elasticsearch is a search engine based on Lucene. It provides a distributed, multitenant-capable full-text search engine with an HTTP web interface and schema-free JSON documents.",
+                getString(R.string.name_elastic),
+                getString(R.string.description_elastic),
                 R.drawable.logo_elastic,
                 37.3864995,
                 -122.0863176,
                 2010,
-                "Shay Banon",
-                "www.elastic.co",
-                "Search Engine",
-                "800 W El Camino Real #350, Mountain View, CA 94040, USA"));
+                getString(R.string.founders_elastic),
+                getString(R.string.site_elastic),
+                getString(R.string.industry_linkedin),
+                getString(R.string.address_elastic)));
         startups.add(new Startup(
-                "Upwork","Upwork, formerly Elance-oDesk, is a global freelancing platform where businesses and independent professionals connect and collaborate remotely.",
+                getString(R.string.name_upwork),
+                getString(R.string.description_upwork),
                 R.drawable.logo_upwork,
                 37.3981541,
                 -122.0512789,
                 2015,
-                "Beerud Sheth and Srini Anumolu",
-                "www.upwork.com",
-                "Freelance marketplace",
-                "441 Logue Ave, Mountain View, CA 94043, USA"));
+                getString(R.string.founders_upwork),
+                getString(R.string.site_upwork),
+                getString(R.string.industry_upwork),
+                getString(R.string.address_upwork)));
         startups.add(new Startup(
-                "Livongo",
-                "Livongo is empowering people with chronic conditions to live a better life.",
+                getString(R.string.name_livongo),
+                getString(R.string.description_livongo),
                 R.drawable.logo_livongo,
                 37.3981541,
                 -122.0512789,
                 2014,
-                "Kimon Angelides",
-                "www.livongo.com",
-                "Health Care, Health Diagnostics, Information Technology, Wellness",
-                "441 Logue Ave, Mountain View, CA 94043, USA"));
+                getString(R.string.founders_livongo),
+                getString(R.string.site_livongo),
+                getString(R.string.industry_livongo),
+                getString(R.string.address_livongo)));
         startups.add(new Startup(
-                "Punchh",
-                "Punchh offers digital marketing products combining AI and machine learning technologies.",
+                getString(R.string.name_punchh),
+                getString(R.string.description_punchh),
                 R.drawable.logo_punchh,
                 37.4065132,
                 -122.1112712,
                 2010,
-                "Aditya Sanghi, Jitendra Gupta, Sastry Penumarthy, Shyam Rao",
-                "www.punchh.com",
-                "Digital Marketing",
-                "201 San Antonio Cir #250, Mountain View, CA 94040, USA"));
+                getString(R.string.founders_punchh),
+                getString(R.string.site_punchh),
+                getString(R.string.industry_punchh),
+                getString(R.string.address_punchh)));
         startups.add(new Startup(
-                "Pure Storage",
-                "Pure Storage is enterprise storage company for broad deployment of flash in data centers.",
+                getString(R.string.name_pure_storage),
+                getString(R.string.description_pure_storage),
                 R.drawable.logo_pure_storage,
                 37.3881372,
                 -122.0851207,
                 2009,
-                "John Colgrove, John Hayes",
-                "www.purestorage.com",
-                "Cloud Computing, Data Storage, Enterprise Software",
-                "650 Castro St #400, Mountain View, CA 94041, USA"));
+                getString(R.string.founders_pure_storage),
+                getString(R.string.site_pure_storage),
+                getString(R.string.industry_pure_storage),
+                getString(R.string.address_pure_storage)));
 
         StartupAdapter adapter = new StartupAdapter(getActivity(), startups, R.id.image_view_item_list);
         ListView listView = rootView.findViewById(R.id.list_view);
